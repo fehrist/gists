f27e662327eaaf687ce4993b582f2f616e01453e
Mustafa Magdi <developer.mustafa@gmail.com>
2024-09-25 15:39:22 +0300

    add zid test script

--- zid.js
+++ zid.js
@@ -1,5 +1,5 @@
 var test_fehris = 'Test Fehris integration';
 const test_const_fehris = "Test const";
 
-console.log('Hello from fehris');
+console.log('Hello from fehris!');
 document.querySelector('.fehrisPagination__info').style.display = 'none';
